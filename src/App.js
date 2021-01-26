1eea792a5c0580634f758087683477a90a04309c
yanghm <code004@gmail.com>
2021-01-26 17:05:55 +0900

    link 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
@@ -7,6 +7,15 @@ import Home from "./components/Home";
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </div>
